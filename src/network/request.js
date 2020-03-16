68e94c002ee27241cebd99420198fce138cc08ff
RRJw <2463781102@qq.com>
2020-03-16 23:51:06 +0800

    2020.3.16

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@
 
   export function request(config) {
       const instance = axios.create({
-          baseURL: 'http://123.207.32.32:8000/api/v1',
+          baseURL: 'http://106.54.54.237:8000/api/v1',
           timeout: 5000
       })
 
